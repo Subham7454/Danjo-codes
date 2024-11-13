99ce300614773775375c282f6a719c75ff46e434
Subham <shubhamrawat1843@gmail.com>
2024-11-13 18:44:33 +0530

    model and iamge settings add

--- dangoProject/chai/models.py
+++ dangoProject/chai/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ChaiVriety(models.Model):
+
+    CHAI_TYPE_CHOICE=[
+    ('ML','MASALA'),    
+    ('GR','GINGER'),    
+    ('KL','KIWI'),    
+    ('PL','PALIN'),    
+    ('EL','ELAICHI'),    
+     ]    
+
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='chais/')
+    date_aded=models.DateTimeField(default=timezone.now())
+    type=models.CharField(max_length=2,choices=CHAI_TYPE_CHOICE)
